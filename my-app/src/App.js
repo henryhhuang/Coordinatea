fd32ea2ba33dc2731c76bc5ad95cb402fbe35cb0
DapperQuokka <justin.tran.jjt@outlook.com>
2022-03-13 23:57:03 -0400

    modify front end app to integrate with backend graphql server (apollo client), add cookie utility for authentication

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,14 @@ import { CreateJourney } from './components/CreateJourney/CreateJourney';
 // import NavBar from './components/NavBar/NavBar';
 import { ViewJourney } from './components/ViewJourney/ViewJourney';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { SignIn } from './components/SignIn/SignIn';
+import { SignUp } from './components/SignUp/SignUp';
+import { Comment } from './components/Comments/Comment/Comment';
+import { CommentForm } from './components/Comments/CommentForm/CommentForm';
+import { Follow } from './components/Follow/Follow';
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache, concat, ApolloProvider } from "@apollo/client";
+import { useAuthToken } from "./util/authentication";
+import { CommentList } from './components/Comments/CommentList/CommentList';
 
 const theme = createTheme({
   palette: {
@@ -23,13 +31,32 @@ const theme = createTheme({
 
 function App() {
 
+  const token = useAuthToken();
+
+  const apolloClient = new ApolloClient({
+    uri: 'http://localhost:5000/',
+    cache: new InMemoryCache(),
+    headers: {
+      authorization: token[0]
+    }
+  })
+
   return (
-    <ThemeProvider theme={theme}>
-      <div className='App'>
-        {/* <NavBar></NavBar> */}
-        <ViewJourney></ViewJourney>
-      </div>
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider theme={theme}>
+        <div className='App'>
+          {/*<NavBar></NavBar><Comment />*/}
+          <ViewJourney></ViewJourney>
+          <SignIn></SignIn>
+          <SignUp></SignUp>
+          <Follow />
+          <Comment username={"DapperQuokka"} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"} />
+          <Comment username={"DapperQuokka"} content={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"} />
+          <CommentForm />
+          <CommentList username={'A'} />
+        </div>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 
 }
