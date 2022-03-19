56cf62041b2b7da6d843c95082b9776531ccdea5
Justin Tran <justin.tran.jjt@outlook.com>
2022-03-19 12:00:11 -0400

    implement comment resolvers

---
+++ backend/graphql/resolvers/commentResolvers.js
@@ -0,0 +1,60 @@
+const Comment = require('../../models/Comment');
+const User = require('../../models/User');
+const Journey = require('../../models/Journey');
+
+const commentResolvers = {
+    Query: {
+        getUserComments: async (_, { id }, context) => {
+            console.log(context.req.session);
+            const user = await User.findById(id);
+            if (!user)
+                throw new Error("User does not exist");
+            const comments = await Comment.find({ userId: id });
+            if (comments)
+                return comments
+            return []
+        },
+        getParentComments: async (_, { id }, context) => {
+            console.log(context.req.session)
+            const journey = await Journey.findById(id);
+            const comment = await Comment.findById(id);
+            if (!(journey || comment))
+                throw new Error("Parent object does not exist");
+            const comments = await Comment.find({ parentId: id });
+            if (comments)
+                return comments
+            return [];
+        }
+    },
+    Mutation: {
+        createComment: async (_, { uid, parentId, content }, context) => {
+            console.log(context.req.session);
+            const user = await User.findById(uid);
+            if (!user)
+                throw new Error("User does not exist");
+            const journey = await Journey.findById(parentId);
+            const comment = await Comment.findById(parentId);
+            if (!(journey || comment))
+                throw new Error("Unable to create comment. Parent does object does not exist.");
+            const newComment = new Comment({
+                userId: uid,
+                parentId: parentId,
+                content: content,
+                createdAt: new Date().toISOString()
+            });
+            await newComment.save();
+            return newComment;
+        },
+        /*
+        removeComment: async (_, { id }, context) => {
+            console.log(context);
+            const comment = await Comment.findById(id);
+            if (!comment)
+                throw new Error("Comment does not exist");
+
+        }
+        */
+    }
+}
+
+module.exports = commentResolvers;
---
+++ backend/models/Comment.js
@@ -0,0 +1,11 @@
+const { default: mongoose } = require("mongoose");
+const { model, Schema } = require("mongoose");
+
+const CommentSchema = new Schema({
+    userId: String,
+    parentId: String,
+    content: String,
+    createdAt: String,
+});
+
+module.exports = model("Comment", CommentSchema);
