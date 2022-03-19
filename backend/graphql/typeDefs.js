d1342e6edf2ecc7ba278d0871cf3064a11caff7d
Justin Tran <justin.tran.jjt@outlook.com>
2022-03-19 11:59:46 -0400

    clean up backend typedefs and resolvers

--- backend/graphql/typeDefs.js
+++ backend/graphql/typeDefs.js
@@ -17,7 +17,58 @@ module.exports = gql`
     }
     type Journey {
         id: ID!
+        username: String!
+        title: String!
+        imageId: String!
+        description: String!
+        fromDate: String!
+        toDate: String!
+    }
+    type Marker {
+        id: ID!
+        journeyId: ID!
+        title: String!
+        place: String!
+        description: String!
+        date: String!
+        longitude: Float!
+        latitude: Float!
+    }
+
+    input JourneyInput {
         username: String
-        createdAt: String
+        title: String
+        imageId: String
+        description: String
+        fromDate: String
+        toDate:String
+    }
+    input MarkerInput {
+        journeyId: ID!
+        title: String!
+        place: String!
+        description: String!
+        date: String!
+        longitude: Float!
+        latitude: Float!
+    }
+
+    type Query {
+        getUserById(id: ID): User!
+        getFollowing(id: ID): [User!]!
+        getUserComments(id: ID): [Comment!]!
+        getParentComments(id: ID): [Comment!]!
+        getUserJourneys(id: ID): [Journey!]!
+        getJourneys: [Journey!]!
+        getJourney(id: ID): Journey!
+        getMarkers(journeyId: ID): [Marker!]!
+    }
+
+    type Mutation {
+        follow(subscriberId: ID, publisherId: ID): [User!]!
+        unfollow(subscriberId: ID, publisherId: ID): [User!]!
+        createComment(uid: ID, parentId: ID, content: String): Comment!
+        createJourney(journey: JourneyInput): Journey
+        createMarker(marker: MarkerInput): Marker
     }
 `
