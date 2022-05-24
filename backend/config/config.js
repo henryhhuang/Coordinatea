05661e363d6da809228febf855a41a1e79d35168
Henry Huang <45514926+henryhhuang@users.noreply.github.com>
2022-05-24 15:00:46 -0400

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,2 @@
 module.exports = {
-    MONGODB: "mongodb+srv://admin:O0MRYOe9qNyliBAx@cluster0.w5qep.mongodb.net/coordinate?retryWrites=true&w=majority",
-    SESSION_SECRET: "yueII0ylBNkSn2H86hKF9ucXJMGCoMWVBHfny1V8A1jKdZHHdN",
-    SALT_ROUNDS: 12,
-    MAPBOX_KEY: 'pk.eyJ1IjoiZGFwcGVycXVva2thIiwiYSI6ImNsMGgzZmdmazA0dm4zaW1qcjNmanhtNHYifQ.ODWLrgo7OhsbqXzGMsz1ug',
-    OPENTRIPS_API: '5ae2e3f221c38a28845f05b695cc9d4d80023a7c11186c8d6596d94a'
 }
